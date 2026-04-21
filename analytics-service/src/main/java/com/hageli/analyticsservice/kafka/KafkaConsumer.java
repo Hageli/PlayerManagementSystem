19b878248029b925aa04ee8290e0e78b9c0e8f75
Hageli <miro.hageli@gmail.com>
2026-04-21 14:16:34 +0300

    Kafka connection to analytics service added

---
+++ analytics-service/src/main/java/com/hageli/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,26 @@
+package com.hageli.analyticsservice.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import player.events.PlayerEvent;
+
+
+
+
+@Service
+public class KafkaConsumer {
+    private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics="player", groupId="analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            PlayerEvent playerEvent = PlayerEvent.parseFrom(event);
+            log.info("PlayerCreated event received: [PlayerId={}, name={}, email={}]", playerEvent.getPlayerId(), playerEvent.getName(), playerEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event {}", e.getMessage());
+        }
+    }
+}
---
+++ analytics-service/src/test/java/com/hageli/analyticsservice/AnalyticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hageli.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
